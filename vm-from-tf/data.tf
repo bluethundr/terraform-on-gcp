da1b07d9defa626c8dcc011d833cb9533fb7e266
Tim Dunphy <19714402+bluethundr@users.noreply.github.com>
2025-06-04 13:41:57 -0400

    Working gcs, sa and vm tf scripts.

--- vm-from-tf/data.tf
+++ vm-from-tf/data.tf
@@ -1,7 +1,8 @@
 data "terraform_remote_state" "sa_state" {
   backend = "gcs"
   config = {
-    bucket = "my-terraform-state-bucket-461115" # Must match the bucket used in sa-from-tf
+    bucket = "my-terraform-state-bucket-2555555" # Must match the bucket used in sa-from-tf
     prefix = "terraform/sa-from-tf"             # Must match the prefix used in sa-from-tf
   }
 }
+
