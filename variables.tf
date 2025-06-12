54682e2a825bc50f2f5bffc1d603b41f4e9cf863
Tim Dunphy <19714402+bluethundr@users.noreply.github.com>
2025-06-12 17:28:07 -0400

    Updated the root module. Will expand later.

--- variables.tf
+++ variables.tf
@@ -9,6 +9,22 @@ variable "location" {
 
 }
 
-variable "name" {
+variable "bucket_name" {
   type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "storage_class" {
+  type = string
+}
+
+variable "uniform_bucket_level_access" {
+  type = bool
+}
+
+variable "force_destroy" {
+  type = bool
 }
